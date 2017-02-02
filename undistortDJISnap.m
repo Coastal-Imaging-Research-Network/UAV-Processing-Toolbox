ea3e4a23032aff5cdb9abdfa596a3f012837fa06
jstanleyx <jstanleyx@users.noreply.github.com>
2017-02-02 12:01:23 -0800

    Add files via upload

--- undistortDJISnap.m
+++ undistortDJISnap.m
@@ -13,4 +13,28 @@ foo = interp2(double(squeeze(I(:,:,2))),Ud,Vd);
 I2(:,:,2) = reshape(foo,size(U));
 foo = interp2(double(squeeze(I(:,:,3))),Ud,Vd);
 I2(:,:,3) = reshape(foo,size(U));
-I2 = I2/255;
+
+%
+%   Copyright (C) 2017  Coastal Imaging Research Network
+%                       and Oregon State University
+
+%    This program is free software: you can redistribute it and/or  
+%    modify it under the terms of the GNU General Public License as 
+%    published by the Free Software Foundation, version 3 of the 
+%    License.
+
+%    This program is distributed in the hope that it will be useful,
+%    but WITHOUT ANY WARRANTY; without even the implied warranty of
+%    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+%    GNU General Public License for more details.
+
+%    You should have received a copy of the GNU General Public License
+%    along with this program.  If not, see
+%                                <http://www.gnu.org/licenses/>.
+
+% CIRN: https://coastal-imaging-research-network.github.io/
+% CIL:  http://cil-www.coas.oregonstate.edu
+%
+%key UAVProcessingToolbox
+%
+
